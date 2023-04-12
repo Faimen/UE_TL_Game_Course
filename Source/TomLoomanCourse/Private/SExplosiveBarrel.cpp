801d92d7cb460e57ef6007873c3ef54b74a7de49
Artem Kalugin <kalugin@skytecgames.ru>
2023-04-13 04:42:24 +0700

    Added explosive barrel & Jump

---
+++ Source/TomLoomanCourse/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+
+#include "Components/CapsuleComponent.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("MeshComponent");
+	MeshComponent->SetSimulatePhysics(true);
+	MeshComponent->SetCollisionProfileName("PhysicsActor");
+	RootComponent = MeshComponent;
+
+	RadialForce = CreateDefaultSubobject<URadialForceComponent>("RadialForce");
+	RadialForce->SetupAttachment(RootComponent);
+	RadialForce->Radius = 750.0f;
+	RadialForce->ImpulseStrength = 1500.0f;
+	RadialForce->bImpulseVelChange = true;
+
+	RadialForce->AddCollisionChannelToAffect(ECC_WorldDynamic);
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ASExplosiveBarrel::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	
+	MeshComponent->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnHitCallback);
+}
+
+// Called every frame
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASExplosiveBarrel::OnHitCallback(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	RadialForce->FireImpulse();
+	
+	// Logging to make sure we reached the event
+	UE_LOG(LogTemp, Log, TEXT("OnActorHit reached (ExplosiveBarrel)"));
+}
+
